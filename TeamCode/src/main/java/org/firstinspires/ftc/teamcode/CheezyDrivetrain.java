79a7203a3c7e95e4e8d74df80b92d81fd6e020b9
CannoliBroth <brorpei@gmail.com>
2018-11-06 21:03:14 -0500

    Drivetrain added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CheezyDrivetrain.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+public class CheezyDrivetrain {
+
+
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    public CheezyDrivetrain(CheezyMap robot){
+
+        this.leftFrontDrive = robot.leftFrontDrive;
+        this.leftBackDrive = robot.leftBackDrive;
+        this.rightBackDrive = robot.rightBackDrive;
+        this.rightFrontDrive = robot.rightFrontDrive;
+    }
+
+    public void drivey(double leftTrigger, double rightTrigger, double leftX){
+        double leftPower    = Range.clip(rightTrigger - leftTrigger + leftX, -1.0, 1.0) ;
+        double rightPower   = Range.clip(rightTrigger - leftTrigger - leftX, -1.0, 1.0) ;
+        leftFrontDrive.setPower(leftPower);
+        rightFrontDrive.setPower(rightPower);
+        leftBackDrive.setPower(leftPower);
+        rightBackDrive.setPower(rightPower);
+        //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CheezyMap.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class CheezyMap {
+
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightFrontDrive = null;
+    public DcMotor leftFrontDrive = null;
+    public DcMotor rightBackDrive = null;
+    public DcMotor intake = null;
+
+    public CheezyMap(HardwareMap hardwareMap){
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftFronty");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFronty");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBacky");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightBacky");
+        intake = hardwareMap.get(DcMotor.class, "Intake");
+    }
+    public void init() {
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        intake.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+}
